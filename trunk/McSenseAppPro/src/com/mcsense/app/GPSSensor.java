2f5f83238e3d4a6eba9f6d9fe9664eaa611fd8b5
tmanoop@gmail.com <tmanoop@gmail.com@cdce0dfa-68b7-d00f-34aa-40322ea6ff12>
2012-02-25 19:31:14 +0000

    This Project is for users having Android OS 4 and higher.

---
+++ trunk/McSenseAppPro/src/com/mcsense/app/GPSSensor.java
@@ -0,0 +1,24 @@
+//package com.mcsense.app;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.location.LocationListener;
+//import android.location.LocationManager;
+//import android.os.Bundle;
+//
+//public class GPSSensor extends Activity {
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		
+//		/* Use the LocationManager class to obtain GPS locations */
+//
+//		LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+//
+//		LocationListener mlocListener = new MyLocationListener(getApplicationContext());
+//
+//		mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+//
+//
+//	}
+//}
