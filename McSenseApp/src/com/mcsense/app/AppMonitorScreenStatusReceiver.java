7cb1671471922c9cd41615fce15a74fc447be7d4
ippatsuman <ippatsuman@gmail.com>
2012-03-22 23:49:21 +0000

    Fixed duplicated thread bug. Added static variable to enable application monitoring.

--- McSenseApp/src/com/mcsense/app/AppMonitorScreenStatusReceiver.java
+++ McSenseApp/src/com/mcsense/app/AppMonitorScreenStatusReceiver.java
@@ -18,21 +18,23 @@ public class AppMonitorScreenStatusReceiver extends BroadcastReceiver {
 
 	private static final String LOGTAG = "AppMonitorScreenStatusReceiver";
 	public static final AtomicBoolean isRegistered = new AtomicBoolean(false);
-
-	public static boolean enableMonitor = false;
+	public static final AtomicBoolean applicationMonitoringEnabled = new AtomicBoolean(true);
 
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
 			Log.d(LOGTAG, "Screen unlock detected");
-			Intent i = new Intent(AppMonitorService.START_ACTION);
-			i.setClass(context, AppMonitorService.class);
-			context.startService(i);
+			if (applicationMonitoringEnabled.get()) {
+				Intent i = new Intent(AppMonitorService.START_ACTION);
+				i.setClass(context, AppMonitorService.class);
+				context.startService(i);
+			}
 		} else if (intent.getAction().equalsIgnoreCase(Intent.ACTION_SCREEN_OFF)) {
 			Log.d(LOGTAG, "Screen lock detected");
-			Log.d(LOGTAG, "Screen unlock detected");
-			Intent i = new Intent(AppMonitorService.STOP_ACTION);
-			i.setClass(context, AppMonitorService.class);
-			context.stopService(i);
+			if (AppMonitorService.isRunning()) {
+				Intent i = new Intent(AppMonitorService.STOP_ACTION);
+				i.setClass(context, AppMonitorService.class);
+				context.stopService(i);
+			}
 		}
 	}
 }
