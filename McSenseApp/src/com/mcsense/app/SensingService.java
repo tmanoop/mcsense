bdd4e79bfe7ed30784e0fad2550026020b506f01
ippatsuman <ippatsuman@gmail.com>
2012-04-04 10:09:13 +0000

    Store accelerometer readings to list to prevent unnecessary file access.

--- McSenseApp/src/com/mcsense/app/SensingService.java
+++ McSenseApp/src/com/mcsense/app/SensingService.java
@@ -304,13 +304,14 @@ public class SensingService extends Service {
 	protected void stopSensing() {
 		if(mSensorManager!=null)
 			mSensorManager.unregisterListener(acelSensorListener,mAccelerometer);
+		AppUtils.writeListToFile(getApplicationContext(), acelSensorListener.result,"sensing_file"+currentTask.getTaskId());
 		mSensorManager = null;
 //        mlocManager.removeUpdates(mlocListener);
 	}
 
 	private void startSensing() {
 		startAccelerometerSensing();
-		acelSensorListener = new AcelSensor(getApplicationContext(),currentTask);
+		acelSensorListener = new AcelSensor(getApplicationContext());
 		mSensorManager.registerListener(acelSensorListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 		
 //		myLocation.getLocation(this, locationResult);
